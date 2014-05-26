36be061f53d84a38121a3ed5abb601b1322896db
Richard Guy <richardpaulguy@gmail.com>
2014-05-26 13:59:10 -0400

    Initial Commit

---
+++ OQLocationTestingTests/OQLocationTestingTests.m
@@ -0,0 +1,34 @@
+//
+//  OQLocationTestingTests.m
+//  OQLocationTestingTests
+//
+//  Created by Richard Guy on 5/26/14.
+//  Copyright (c) 2014 OQ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OQLocationTestingTests : XCTestCase
+
+@end
+
+@implementation OQLocationTestingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
